0a57344025009602653ecf6ed39b2cf4bc473414
nadhiyap <36396218+nadhiyap@users.noreply.github.com>
2018-04-28 10:32:57 +0530

    mmx.py

---
+++ mmx.py
@@ -0,0 +1,26 @@
+n=int(input("enter n"))
+k=int(input("enter k"))
+l=[]
+l1=[]
+s=""
+s1=""
+j=0
+for i in range(n):
+    a=input("enter v")
+    if j<k:
+        s=s+a+"x"
+        j+=1
+    else:
+        j=0
+        s=s+","
+        s=s+a+"x"
+        j+=1
+l=s.split(',')
+print(l)
+mi=len(l[0])
+for i in range(len(l)):
+    if len(l[i])<mi:
+        s1=""
+        s1=s1+str(l[i])
+l1=s1.split('x')
+print(max(l1))
